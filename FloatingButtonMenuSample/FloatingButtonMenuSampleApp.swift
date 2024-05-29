116bbdfc992b47521c9ae98c333ae4d934a7d34c
Juanjo Corbalán <juanjocorbalan@gmail.com>
2024-05-29 20:28:18 +0200

    Initial Commit

---
+++ FloatingButtonMenuSample/FloatingButtonMenuSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingButtonMenuSampleApp.swift
+//  FloatingButtonMenuSample
+//
+//  Created by Juanjo Corbalan on 29/5/24.
+//
+
+import SwiftUI
+
+@main
+struct FloatingButtonMenuSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
